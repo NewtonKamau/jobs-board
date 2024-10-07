e4ad5a2101509b671d66fdfcb77078971d30aca3
NewtonKamau <kamaunewton78@gmail.com>
2024-10-07 13:02:41 +0300

    bring in resource

--- routes/web.php
+++ routes/web.php
@@ -15,14 +15,7 @@ use App\Http\Controllers\JobController;
 */
 
 Route::view('/', 'home');
-Route::get('/jobs', [JobController::class, 'index']);
-Route::get('jobs/create', [JobController::class, 'create']);
 
-Route::get('/jobs/{job}', [JobController::class, 'show']);
+Route::resource('jobs', JobController::class);
 
-Route::post('/jobs', [JobController::class, 'store']);
-Route::get('/jobs/{job}/edit', [JobController::class, 'edit']);
-Route::patch('/jobs/{job}', [JobController::class, 'update']);
-
-Route::delete('/jobs/{job}', [JobController::class, 'destroy']);
 Route::view('/contact', 'contact');
